14ebe8221510badf0cfdeb075b246a4fcaa2788f
shahidullah piaro <shahidullah.piyaro@gmail.com>
2024-05-19 16:02:04 +0600

    implemented crud functionalities

--- djangoapp/api/urls.py
+++ djangoapp/api/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path("blogposts/", views.BlogPostListCreate.as_view(), name="blogpost-view-create"),
+     path(
+        "blogposts/<int:pk>/",
+        views.BlogPostRetrieveUpdateDestory.as_view(),
+        name="update",
+    ),
 ]
--- djangoapp/api/views.py
+++ djangoapp/api/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+from rest_framework.response import Response
 
 
 class BlogPostListCreate(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class BlogPostRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = "pk"
